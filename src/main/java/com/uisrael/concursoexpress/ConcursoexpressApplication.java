9990eea0bb83a7962480d599b9014374decd7a11
Master <Master@labuisrael.edu.ec>
2026-06-15 18:31:13 -0500

    first commit

---
+++ src/main/java/com/uisrael/concursoexpress/ConcursoexpressApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.concursoexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcursoexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcursoexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uisrael/concursoexpress/ConcursoexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.concursoexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursoexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
